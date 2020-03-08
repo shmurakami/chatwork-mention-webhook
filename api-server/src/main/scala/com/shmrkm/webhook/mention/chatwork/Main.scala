c187c35961bb7fa5a2324e391d8787ea3b29319c
shaka <shmurakami1104@gmail.com>
2020-03-08 22:46:45 +0900

    reorganize

--- api-server/src/main/scala/com/shmrkm/webhook/mention/chatwork/Main.scala
+++ api-server/src/main/scala/com/shmrkm/webhook/mention/chatwork/Main.scala
@@ -2,12 +2,9 @@ package com.shmrkm.webhook.mention.chatwork
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.stream.ActorMaterializer
-import akka.http.scaladsl.server.Directives._
 import com.shmrkm.webhook.mention.chatwork.controller.WebhookController
 import com.typesafe.config.Config
-import com.webhook.mention.chatwork.useCase.WebhookUseCase
 
 import scala.io.StdIn
 
