06148fdc23d26db9fd0735e548920dc4e5d28605
shaka <shmurakami1104@gmail.com>
2020-03-28 09:59:11 +0900

    mention read requires chatwork token

--- api-server/src/main/scala/com/shmrkm/chatworkWebhook/mention/controller/MentionController.scala
+++ api-server/src/main/scala/com/shmrkm/chatworkWebhook/mention/controller/MentionController.scala
@@ -4,32 +4,34 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.redis.RedisClient
-import com.shmrkm.chatworkMention.repository.{ ChatworkApiRepositoryImpl, MentionRepositoryRedisImpl }
+import com.shmrkm.chatworkMention.repository.{ChatworkApiRepositoryImpl, MentionRepositoryRedisImpl}
 import com.shmrkm.chatworkWebhook.domain.model.account.ToAccountId
 import com.shmrkm.chatworkWebhook.domain.model.mention.MentionList
 import com.shmrkm.chatworkWebhook.mention.protocol.MentionQuery
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 
 class MentionController(implicit system: ActorSystem) {
 
   import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
   import io.circe.generic.auto._
 
-  private val chatworkApiConfig = system.settings.config.getConfig("chatwork.api")
+  val chatworkApiConfig = system.settings.config.getConfig("chatwork.api")
+  val apiUrl            = chatworkApiConfig.getString("url")
   // don't want to put resource to api-server
-  private val redisConfig = system.settings.config.getConfig("redis")
+  val redisConfig = system.settings.config.getConfig("redis")
 
   def routes: Route =
     extractExecutionContext { implicit ec =>
-      parameters('account_id.as[Int]) { accountId =>
-        onSuccess(execute(MentionQuery(ToAccountId(accountId)))) { response => complete(response) }
+      headerValueByName("X-ChatworkToken") { chatworkToken =>
+        parameters('account_id.as[Int]) { accountId =>
+          onSuccess(execute(MentionQuery(ToAccountId(accountId)), chatworkToken)) { response => complete(response) }
+        }
       }
     }
 
-  def execute(query: MentionQuery)(implicit ec: ExecutionContext): Future[MentionList] = {
-    val apiUrl                = chatworkApiConfig.getString("url")
-    val token                 = chatworkApiConfig.getString("token")
+  def execute(query: MentionQuery, token: String)(implicit ec: ExecutionContext): Future[MentionList] = {
+    // validate token
     val chatworkApiRepository = new ChatworkApiRepositoryImpl(apiUrl, token)
     val mentionRepository = new MentionRepositoryRedisImpl(
       new RedisClient(redisConfig.getString("host"), redisConfig.getInt("port"))
