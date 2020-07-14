2d1df47596190bfd60a9f77542e84bf733cebc01
shaka <shmurakami1104@gmail.com>
2020-07-14 10:40:48 +0900

    not used

--- infrastructure/src/main/scala/com/shmrkm/chatworkMention/repository/AuthenticationRepositoryImpl.scala
+++ infrastructure/src/main/scala/com/shmrkm/chatworkMention/repository/AuthenticationRepositoryImpl.scala
@@ -2,15 +2,14 @@ package com.shmrkm.chatworkMention.repository
 
 import com.redis.RedisClient
 import com.shmrkm.chatworkMention.exception.StoreException
-import com.shmrkm.chatworkMention.hash.{HashHelper, TokenGenerator}
+import com.shmrkm.chatworkMention.hash.TokenGenerator
 import com.shmrkm.chatworkWebhook.domain.model.account.AccountId
 import com.shmrkm.chatworkWebhook.domain.model.auth.{AccessToken, Authentication}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class AuthenticationRepositoryImpl(redisClient: RedisClient)(implicit ex: ExecutionContext)
-    extends AuthenticationRepository
-    with HashHelper {
+    extends AuthenticationRepository {
   import io.circe.generic.auto._
   import io.circe.parser._
   import io.circe.syntax._
