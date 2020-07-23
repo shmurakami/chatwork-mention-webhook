e1066d2d73ac6c7dcea019fc2b73e8762696a10a
shaka <shmurakami1104@gmail.com>
2020-07-23 10:34:51 +0900

    extract extracting body process to function

--- application/api-server/src/main/scala/com/shmrkm/chatworkWebhook/mention/controller/WebhookController.scala
+++ application/api-server/src/main/scala/com/shmrkm/chatworkWebhook/mention/controller/WebhookController.scala
@@ -5,7 +5,7 @@ import java.util.Base64
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.HttpEntity
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.{Directive, Directive0, Directive1, MalformedRequestContentRejection, Route}
+import akka.http.scaladsl.server.{Directive0, MalformedRequestContentRejection, Route}
 import akka.pattern.ask
 import akka.util.{ByteString, Timeout}
 import com.shmrkm.chatworkWebhook.mention.mention.MentionHandlerActor.ReceiveMention
@@ -20,18 +20,16 @@ class WebhookController(implicit system: ActorSystem) extends Controller {
   import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
   import io.circe.generic.auto._
 
-  private val chatworkConfig = system.settings.config.getConfig("chatwork")
+  private val chatworkConfig     = system.settings.config.getConfig("chatwork")
   private val signatureHeaderKey = chatworkConfig.getString("webhook.signature_header_key")
 
   private val hmacSecret = Base64.getDecoder.decode(chatworkConfig.getString("webhook.token"))
 
-  // TODO directive1...
-//  def extractRequestBody(entity: HttpEntity)(implicit ec: ExecutionContext): Directive1[Future[String]] = {
-//    Tuple1(entity.dataBytes.runFold(ByteString.empty)(_ ++ _).map(_.utf8String))
-//  }
+  def extractRequestBody(entity: HttpEntity)(implicit ec: ExecutionContext): Future[String] =
+    entity.dataBytes.runFold(ByteString.empty)(_ ++ _).map(_.utf8String)
 
   def verifySignature(expectSignature: String, body: String): Directive0 = {
-    val hmac = HMACGenerator.generate(hmacSecret, body)
+    val hmac      = HMACGenerator.generate(hmacSecret, body)
     val signature = Base64.getEncoder.encodeToString(hmac)
     if (expectSignature == signature) pass
     else reject(MalformedRequestContentRejection("Invalid Request", new RuntimeException("Invalid Request")))
@@ -45,12 +43,10 @@ class WebhookController(implicit system: ActorSystem) extends Controller {
       headerValueByName(signatureHeaderKey) { applySignature =>
         extractExecutionContext { implicit ec =>
           extractRequestEntity { requestEntity =>
-            onSuccess(requestEntity.dataBytes.runFold(ByteString.empty)(_ ++ _).map(_.utf8String)) { body =>
+            onSuccess(extractRequestBody(requestEntity)) { body =>
               verifySignature(applySignature, body) {
                 entity(as[WebhookRequest]) { request =>
-                  onComplete(execute(request.mentionCommand)) { response =>
-                    complete(response)
-                  }
+                  onComplete(execute(request.mentionCommand)) { response => complete(response) }
                 }
               }
             }
