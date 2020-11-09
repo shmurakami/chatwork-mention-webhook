61f6df0475b8ad95849cf7a27ce7832637b7d857
shaka <shmurakami1104@gmail.com>
2020-11-09 22:49:58 +0900

    test to ensure store recent 200 messages

--- modules/src/test/scala/com/shmrkm/chatworkWebhook/mention/subscriber/MessageSubscriberSpec.scala
+++ modules/src/test/scala/com/shmrkm/chatworkWebhook/mention/subscriber/MessageSubscriberSpec.scala
@@ -1,27 +1,26 @@
 package com.shmrkm.chatworkWebhook.mention.subscriber
 
 import akka.actor.ActorSystem
-import akka.testkit.{ ImplicitSender, TestKit }
+import akka.testkit.{ImplicitSender, TestKit}
 import com.redis.RedisClient
-import com.shmrkm.chatworkMention.repository.{
-  AuthenticationRepository,
-  ChatworkApiRepository,
-  MeResponse,
-  MentionRepositoryRedisImpl
-}
+import com.shmrkm.chatworkMention.repository.{AuthenticationRepository, ChatworkApiRepository, MeResponse, MentionRepositoryRedisImpl}
 import com.shmrkm.chatworkWebhook.concerns.StopSystemAfterAll
-import com.shmrkm.chatworkWebhook.domain.model.account.{ AccountId, AccountName, FromAccount, FromAccountAvatarUrl }
-import com.shmrkm.chatworkWebhook.domain.model.auth.{ AccessToken, Authentication }
-import com.shmrkm.chatworkWebhook.domain.model.chatwork.{ ApiToken, Me }
+import com.shmrkm.chatworkWebhook.domain.model.account.{AccountId, AccountName, FromAccount, FromAccountAvatarUrl}
+import com.shmrkm.chatworkWebhook.domain.model.auth.{AccessToken, Authentication}
+import com.shmrkm.chatworkWebhook.domain.model.chatwork.{ApiToken, Me}
+import com.shmrkm.chatworkWebhook.domain.model.mention.MentionList
 import com.shmrkm.chatworkWebhook.domain.model.message._
-import com.shmrkm.chatworkWebhook.domain.model.room.{ Room, RoomIconUrl, RoomId, RoomName }
+import com.shmrkm.chatworkWebhook.domain.model.query.message.QueryMessage
+import com.shmrkm.chatworkWebhook.domain.model.room.{Room, RoomIconUrl, RoomId, RoomName}
 import com.shmrkm.chatworkWebhook.mention.subscriber.MessageSubscriber.ConsumedMessage
 import com.typesafe.config.ConfigFactory
+import org.scalatest.concurrent.PatienceConfiguration.{Interval, Timeout}
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.wordspec.AnyWordSpecLike
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
 import scala.util.Try
 
@@ -69,20 +68,22 @@ class MessageSubscriberSpec
 
   "subscriber" when {
     "got ConsumedMessage" should {
+      import io.circe.generic.auto._
+      import io.circe.syntax._
+
+      val message = Message(
+        id = MessageId("1"),
+        roomId = roomId,
+        fromAccountId = fromAccountId,
+        toAccountId = toAccountId,
+        body = MessageBody("hello"),
+        sendTime = SendTime(1593593046),
+        updateTime = UpdateTime(0)
+      )
+
+      val messageJsonString = message.asJson.toString
+
       "run update readmodel flow" in {
-        import io.circe.generic.auto._
-        import io.circe.syntax._
-
-        val messageJsonString = Message(
-          id = MessageId("1"),
-          roomId = roomId,
-          fromAccountId = fromAccountId,
-          toAccountId = toAccountId,
-          body = MessageBody("hello"),
-          sendTime = SendTime(1593593046),
-          updateTime = UpdateTime(0)
-        ).asJson.toString
-
         implicit val ec: ExecutionContext = system.dispatcher
 
         val mentionRepository = new MentionRepositoryRedisImpl(
@@ -106,6 +107,42 @@ class MessageSubscriberSpec
         val mentionList = mentionRepository.resolve(toAccountId).futureValue
         mentionList.list.length shouldBe 1
         mentionList.list.head.body shouldBe MessageBody("hello")
+      }
+
+      "store up to 200 items" in {
+        implicit val ec: ExecutionContext = system.dispatcher
+
+        val mentionRepository = new MentionRepositoryRedisImpl(
+          new RedisClient(config.getString("redis.host"), config.getInt("redis.port"))
+        )
+
+        val queryMessage = QueryMessage(
+          id = message.id,
+          roomId = message.roomId,
+          roomName = RoomName(""),
+          roomIconUrl = RoomIconUrl(""),
+          fromAccountId = message.fromAccountId,
+          fromAccountName = AccountName(""),
+          fromAccountAvatarUrl = FromAccountAvatarUrl(""),
+          toAccountId = message.toAccountId,
+          body = message.body,
+          sendTime = message.sendTime,
+          updateTime = message.updateTime
+        )
+
+        val list = (0 to 200).foldLeft(Seq.empty[QueryMessage])((list, _) => list :+ queryMessage)
+        val storeMentionList = MentionList(list)
+
+        // save 200 items. and then consume 1 message then returns only 200 items
+        mentionRepository.updateReadModel(toAccountId, storeMentionList)
+
+        val subscriber = system.actorOf(MessageSubscriberWorker.props(authenticationRepository, mentionRepository, chatworkApiRepository), "subscriber")
+        subscriber ! ConsumedMessage(messageJsonString)
+
+        expectNoMessage(1000 milliseconds)
+
+        val mentionList = mentionRepository.resolve(toAccountId).futureValue(Timeout(Span(10, Seconds)))
+        mentionList.list.length shouldBe 200
       }
     }
   }
