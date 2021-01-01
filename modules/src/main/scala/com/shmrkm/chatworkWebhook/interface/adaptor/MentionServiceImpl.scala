308e398a38f65214862bc83a9ee1749800ddf423
shaka <shmurakami1104@gmail.com>
2021-01-01 21:35:24 +0900

    convert subscribe message to reply

--- modules/src/main/scala/com/shmrkm/chatworkWebhook/interface/adaptor/MentionServiceImpl.scala
+++ modules/src/main/scala/com/shmrkm/chatworkWebhook/interface/adaptor/MentionServiceImpl.scala
@@ -2,56 +2,68 @@ package com.shmrkm.chatworkWebhook.interface.adaptor
 
 import akka.NotUsed
 import akka.stream.scaladsl.Source
-import com.redis._
-import com.shmrkm.chatworkMention.repository.{MentionStreamRepositoryFactory, StreamConsumer}
+import com.shmrkm.chatworkMention.repository.{ MentionStreamRepositoryFactory, StreamConsumer }
 import com.shmrkm.chatworkWebhook.domain.model.account.AccountId
 import com.shmrkm.chatworkWebhook.domain.model.query.message.QueryMessage
 import com.shmrkm.chatworkWebhook.mention.protocol.query.MentionQuery
 import com.shmrkm.chatworkWebhook.mention.usecase.MentionListUseCase
-import org.reactivestreams.{Publisher, Subscriber}
+import org.reactivestreams.{ Publisher, Subscriber }
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
-class MentionServiceImpl[T](mentionListUseCase: MentionListUseCase)(implicit val ec: ExecutionContext) extends MentionService with MentionStreamRepositoryFactory {
+// TODO create new proto for subscribe method
+class MentionServiceImpl[T](mentionListUseCase: MentionListUseCase, _publisher: Publisher[String] = null)(implicit val ec: ExecutionContext)
+    extends MentionService
+    with MentionStreamRepositoryFactory {
 
   override def list(in: MentionListRequest): Future[MentionListReply] = {
     mentionListUseCase.execute(MentionQuery(AccountId(in.accountId))).map {
       case Right(mentionListResponse) =>
-        MentionListReply(mentionListResponse.mentionList.list.map { queryMessage: QueryMessage =>
-          MentionReply(
-            id = queryMessage.id.value,
-            roomId = queryMessage.roomId.value,
-            roomName = queryMessage.roomName.value,
-            roomIconUrl = queryMessage.roomIconUrl.value,
-            fromAccountId = queryMessage.fromAccountId.value.toInt,
-            fromAccountName = queryMessage.fromAccountName.value,
-            fromAccountAvatarUrl = queryMessage.fromAccountAvatarUrl.value,
-            toAccountId = queryMessage.toAccountId.value.toInt,
-            body = queryMessage.body.value,
-            sendTime = queryMessage.sendTime.value,
-            updateTime = queryMessage.updateTime.value
-          )
-        })
+        MentionListReply(mentionListResponse.mentionList.list.map(queryMessage2MentionReply))
       case Left(error) => throw new Exception(error)
     }
   }
 
   override def subscribe(in: MentionSubscribeRequest): Source[MentionReply, NotUsed] = {
-    val mentionStreamRepository = factoryStreamRepository()
-
-    val publisher = new Publisher[String] {
-      override def subscribe(s: Subscriber[_ >: String]): Unit = {
-        mentionStreamRepository.subscribePushNotification(new StreamConsumer {
-          override def onSubscribe(channel: String): Unit = ()
-          override def onUnsubscribe(channel: String): Unit = ()
-          override def onMessage(message: String): Unit = s.onNext(message)
-          override def onError(e: Throwable): Unit = s.onError(e)
-        })
+    val publisher: Publisher[String] = _publisher match {
+      case null => new Publisher[String] {
+        override def subscribe(s: Subscriber[_ >: String]): Unit = {
+          val mentionStreamRepository = factoryStreamRepository()
+          mentionStreamRepository.subscribePushNotification(new StreamConsumer {
+            override def onSubscribe(channel: String): Unit   = ()
+            override def onUnsubscribe(channel: String): Unit = ()
+            override def onMessage(message: String): Unit     = s.onNext(message)
+            override def onError(e: Throwable): Unit          = s.onError(e)
+          })
+        }
       }
+      case p => p
     }
 
     Source.fromPublisher(publisher).map { message =>
-      MentionReply()
+      import io.circe.parser._
+      import io.circe.generic.auto._
+      decode[QueryMessage](message) match {
+        // TODO check account ID. actually should subscribing filter with given account id
+        case Right(queryMessage) => queryMessage2MentionReply(queryMessage)
+        case Left(e)             => throw e
+      }
     }
+  }
+
+  private def queryMessage2MentionReply(queryMessage: QueryMessage): MentionReply = {
+    MentionReply(
+      id = queryMessage.id.value,
+      roomId = queryMessage.roomId.value,
+      roomName = queryMessage.roomName.value,
+      roomIconUrl = queryMessage.roomIconUrl.value,
+      fromAccountId = queryMessage.fromAccountId.value.toInt,
+      fromAccountName = queryMessage.fromAccountName.value,
+      fromAccountAvatarUrl = queryMessage.fromAccountAvatarUrl.value,
+      toAccountId = queryMessage.toAccountId.value.toInt,
+      body = queryMessage.body.value,
+      sendTime = queryMessage.sendTime.value,
+      updateTime = queryMessage.updateTime.value
+    )
   }
 }
--- modules/src/test/scala/com/shmrkm/chatworkWebhook/interface/adaptor/MentionServiceImplSpec.scala
+++ modules/src/test/scala/com/shmrkm/chatworkWebhook/interface/adaptor/MentionServiceImplSpec.scala
@@ -1,25 +1,55 @@
 package com.shmrkm.chatworkWebhook.interface.adaptor
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.model.{HttpEntity, HttpMessage, HttpMethod, HttpMethods, HttpRequest}
+import akka.http.scaladsl.model.{ HttpEntity, HttpMessage, HttpMethod, HttpMethods, HttpRequest }
+import akka.stream.scaladsl.Sink
 import akka.testkit.TestKit
-import com.shmrkm.chatworkWebhook.domain.model.account.{AccountId, AccountName, FromAccountAvatarUrl}
+import com.shmrkm.chatworkWebhook.domain.model.account.{ AccountId, AccountName, FromAccountAvatarUrl }
 import com.shmrkm.chatworkWebhook.domain.model.mention.MentionList
-import com.shmrkm.chatworkWebhook.domain.model.message.{MessageBody, MessageId, SendTime, UpdateTime}
+import com.shmrkm.chatworkWebhook.domain.model.message.{ MessageBody, MessageId, SendTime, UpdateTime }
 import com.shmrkm.chatworkWebhook.domain.model.query.message.QueryMessage
-import com.shmrkm.chatworkWebhook.domain.model.room.{RoomIconUrl, RoomId, RoomName}
-import com.shmrkm.chatworkWebhook.mention.protocol.query.{MentionListResponse, MentionQuery}
+import com.shmrkm.chatworkWebhook.domain.model.room.{ RoomIconUrl, RoomId, RoomName }
+import com.shmrkm.chatworkWebhook.mention.protocol.query.{ MentionListResponse, MentionQuery }
 import com.shmrkm.chatworkWebhook.mention.usecase.MentionListUseCase
-import org.reactivestreams.{Publisher, Subscriber, Subscription}
-import org.scalatest.concurrent.PatienceConfiguration.{Interval, Timeout}
+import org.reactivestreams.{ Publisher, Subscriber, Subscription }
+import org.scalatest.concurrent.PatienceConfiguration.{ Interval, Timeout }
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
-import org.scalatest.time.{Seconds, Span}
+import org.scalatest.time.{ Seconds, Span }
 import org.scalatest.wordspec.AnyWordSpecLike
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
 class MentionServiceImplSpec extends TestKit(ActorSystem()) with AnyWordSpecLike with Matchers with ScalaFutures {
+
+  val queryMessages: Vector[QueryMessage] = Vector(
+    QueryMessage(
+      id = MessageId("1"),
+      roomId = RoomId(2L),
+      roomName = RoomName("room2"),
+      roomIconUrl = RoomIconUrl("roomIcon2"),
+      fromAccountId = AccountId(3),
+      fromAccountName = AccountName("from3"),
+      fromAccountAvatarUrl = FromAccountAvatarUrl("fromAvatar3"),
+      toAccountId = AccountId(1),
+      body = MessageBody("body1"),
+      sendTime = SendTime(0),
+      updateTime = UpdateTime(0)
+    ),
+    QueryMessage(
+      id = MessageId("10"),
+      roomId = RoomId(20L),
+      roomName = RoomName("room20"),
+      roomIconUrl = RoomIconUrl("roomIcon20"),
+      fromAccountId = AccountId(30),
+      fromAccountName = AccountName("from30"),
+      fromAccountAvatarUrl = FromAccountAvatarUrl("fromAvatar30"),
+      toAccountId = AccountId(1),
+      body = MessageBody("body10"),
+      sendTime = SendTime(0),
+      updateTime = UpdateTime(0)
+    )
+  )
 
   "MentionService" should {
     "return list of mention" in {
@@ -29,36 +59,7 @@ class MentionServiceImplSpec extends TestKit(ActorSystem()) with AnyWordSpecLike
         Future {
           Right(
             MentionListResponse(
-              MentionList(
-                Vector(
-                  QueryMessage(
-                    id = MessageId("1"),
-                    roomId = RoomId(2L),
-                    roomName = RoomName("room2"),
-                    roomIconUrl = RoomIconUrl("roomIcon2"),
-                    fromAccountId = AccountId(3),
-                    fromAccountName = AccountName("from3"),
-                    fromAccountAvatarUrl = FromAccountAvatarUrl("fromAvatar3"),
-                    toAccountId = AccountId(1),
-                    body = MessageBody("body1"),
-                    sendTime = SendTime(0),
-                    updateTime = UpdateTime(0)
-                  ),
-                  QueryMessage(
-                    id = MessageId("10"),
-                    roomId = RoomId(20L),
-                    roomName = RoomName("room20"),
-                    roomIconUrl = RoomIconUrl("roomIcon20"),
-                    fromAccountId = AccountId(30),
-                    fromAccountName = AccountName("from30"),
-                    fromAccountAvatarUrl = FromAccountAvatarUrl("fromAvatar30"),
-                    toAccountId = AccountId(1),
-                    body = MessageBody("body10"),
-                    sendTime = SendTime(0),
-                    updateTime = UpdateTime(0)
-                  )
-                )
-              )
+              MentionList(queryMessages)
             )
           )
         }
@@ -97,47 +98,44 @@ class MentionServiceImplSpec extends TestKit(ActorSystem()) with AnyWordSpecLike
     }
 
     "return stream of new mention" in {
+      import io.circe.syntax._
+      import io.circe.generic.auto._
+
       implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
 
-      val mentionListUseCase: MentionListUseCase = (query: MentionQuery) => Future {
-        Right(MentionListResponse(MentionList(Seq.empty)))
-      }
-
-      val publisher: Publisher[Int] = (s: Subscriber[_ >: Int]) => {
-        s.onSubscribe(new Subscription {
-          override def request(n: Long): Unit = println(s"on request $n $s")
-          override def cancel(): Unit = println(s"on cancel $s")
-        })
-
-//        (0 to 7).foreach { num =>
-//          s.onNext(100)
-////          if (s eq null) s.onComplete()
-////          else s.onNext(num)
-//        }
-
+      val mentionListUseCase: MentionListUseCase = (query: MentionQuery) =>
         Future {
-          Thread.sleep(500L)
-          s.onNext(1)
+          Right(MentionListResponse(MentionList(Seq.empty)))
         }
 
-//        s.onComplete()
+      val publisher = new Publisher[String] {
+        override def subscribe(s: Subscriber[_ >: String]): Unit = {
+          // give json string will be parsed to object and then reply
+          val queryMessageAsString = queryMessages.head.asJson.noSpaces
+          s.onNext(queryMessageAsString)
+
+          s.onComplete()
+        }
       }
 
-      val mentionService = new MentionServiceImpl(mentionListUseCase)
-//      val mentionService = MentionServiceHandler.partial(new MentionServiceImpl(mentionListUseCase, publisher))
-//      val reply = mentionService.apply(HttpRequest(
-//        method = HttpMethods.POST,
-//        uri = s"/${MentionService.name}/subscribe",
-//        entity = HttpEntity("account_id=1")
-//      )).futureValue
-//
-//      reply shouldBe null
+      val mentionService = new MentionServiceImpl(mentionListUseCase, publisher)
 
-      val source = mentionService.subscribe(MentionSubscribeRequest(accountId = 1))
-      val f = source.runForeach { out => println(s"got reply $out") }
-      whenReady(f, Timeout(Span(10L, Seconds)), Interval(Span(1L, Seconds))) {
-        _ => println("done")
-      }
+      val source       = mentionService.subscribe(MentionSubscribeRequest(accountId = 1))
+      val mentionReply = source.runWith(Sink.head).futureValue(Timeout(Span(5L, Seconds)), Interval(Span(1L, Seconds)))
+      val expect = MentionReply(
+        id = "1",
+        roomId = 2L,
+        roomName = "room2",
+        roomIconUrl = "roomIcon2",
+        fromAccountId = 3,
+        fromAccountName = "from3",
+        fromAccountAvatarUrl = "fromAvatar3",
+        toAccountId = 1,
+        body = "body1",
+        sendTime = 0,
+        updateTime = 0
+      )
+      mentionReply shouldEqual expect
     }
   }
 
