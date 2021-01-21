570e634390cd07eda1f9babbfa7d733b2ce4e2e3
shaka <shmurakami1104@gmail.com>
2021-01-21 12:34:58 +0900

    extract token authorization

--- application/api-server/src/main/scala/com/shmrkm/chatworkWebhook/mention/controller/MentionController.scala
+++ application/api-server/src/main/scala/com/shmrkm/chatworkWebhook/mention/controller/MentionController.scala
@@ -5,41 +5,32 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.shmrkm.chatworkMention.repository.{AuthenticationRepository, AuthenticationRepositoryFactory}
-import com.shmrkm.chatworkWebhook.auth.exception.AuthenticationFailureException
 import com.shmrkm.chatworkWebhook.domain.model.account.AccountId
-import com.shmrkm.chatworkWebhook.domain.model.auth.Authentication
+import com.shmrkm.chatworkWebhook.mention.directive.TokenAuthorizationDirective
 import com.shmrkm.chatworkWebhook.mention.protocol.query
 import com.shmrkm.chatworkWebhook.mention.usecase.MentionListUseCase
 import com.typesafe.scalalogging.Logger
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success}
 
 class MentionController(mentionListUseCase: MentionListUseCase)(implicit system: ActorSystem)
     extends Controller
+    with TokenAuthorizationDirective
     with AuthenticationRepositoryFactory {
-
-  override implicit def ec: ExecutionContext = system.dispatcher
 
   import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 
-  private val authRepository: AuthenticationRepository = factoryAuthenticationRepository()
+  override implicit def ec: ExecutionContext = system.dispatcher
+  implicit def authRepository: AuthenticationRepository = factoryAuthenticationRepository()
   private val logger                                   = Logger(classOf[MentionController])
-
-  def verifyAccessToken(requestToken: String, accountId: AccountId): Future[Authentication] = {
-    authRepository.resolve(accountId).flatMap {
-      case Right(authentication) if authentication.accountId == accountId && authentication.accessToken.value == requestToken => Future.successful(authentication)
-      case Right(_) => Future.failed(AuthenticationFailureException())
-      case Left(ex: Throwable) => Future.failed(AuthenticationFailureException())
-    }
-  }
 
   def route: Route =
     get {
       extractExecutionContext { implicit ec =>
         parameters('account_id.as[Int]) { accountId =>
-          headerValueByName("X-Token") { token =>
-            onComplete(verifyAccessToken(token, AccountId(accountId))) {
+          tokenAuthorization(AccountId(accountId)) { futureAuthentication =>
+            onComplete(futureAuthentication) {
               case Failure(ex) =>
                 logger.warn(s"failure to resolve authentication $ex")
                 complete("""{"error": "unauthorized}""")
